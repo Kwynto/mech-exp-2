02c4d01a9e7eb0d23fabe2391d6fb87d7e8e18e6
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2026-03-05 15:13:11 +0300

    fyne init

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
 func main() {
 
+	fyneApp := app.New()
+	mainWindow := fyneApp.NewWindow("Мечталлион 2 - анализатор")
+	mainWindow.Resize(fyne.NewSize(400, 100))
+	mainWindow.SetFixedSize(true)
+
+	btnShowDB := widget.NewButton("Показать базу тиражей", func() {
+		fmt.Println("Типа показали базу.")
+	})
+
+	btnAddGame := widget.NewButton("Добавить данные тиража", func() {
+		fmt.Println("Типа добавили данные.")
+	})
+
+	btnAnalize01 := widget.NewButton("Дефектный анализ", func() {
+		fmt.Println("Типа сделали дефектный анализ.")
+	})
+
+	mainWindow.SetContent(container.NewVBox(
+		btnShowDB,
+		btnAddGame,
+		btnAnalize01,
+	))
+
+	mainWindow.ShowAndRun()
 }
