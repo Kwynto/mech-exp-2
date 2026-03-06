2d6d6512541578253881985c866f91f39c0c1baf
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2026-03-06 14:30:22 +0300

    Overview of the circulation database

--- main.go
+++ main.go
@@ -101,12 +101,48 @@ var btnDefAnalize *widget.Button = widget.NewButton("Дефектный анал
 // Окно показа базы
 var showDBWindow fyne.Window = fyneApp.NewWindow("Мечталлион 2 - анализатор - архив тиражей")
 
+var showDBTable *widget.Table = widget.NewTable(
+	func() (rows int, cols int) {
+		return len(SlStGames), 3
+	},
+
+	func() fyne.CanvasObject {
+		return widget.NewLabel("   ")
+	},
+
+	func(tci widget.TableCellID, co fyne.CanvasObject) {
+		co.(*widget.Label).SetText(func() string {
+			var sTemp string
+			switch tci.Col {
+			case 0:
+				sTemp = fmt.Sprintf("%d", SlStGames[tci.Row].Game)
+			case 1:
+				sTemp = func(inArr []int) string {
+					var sT string
+					for _, v := range inArr {
+						sT = fmt.Sprintf("%s %d", sT, v)
+					}
+					return sT
+				}(SlStGames[tci.Row].Wins)
+			case 2:
+				sTemp = func(inArr []int) string {
+					var sT string
+					for _, v := range inArr {
+						sT = fmt.Sprintf("%s %d", sT, v)
+					}
+					return sT
+				}(SlStGames[tci.Row].Wrong)
+			}
+			return sTemp
+		}())
+	},
+)
+
 func showGameDB() {
 	readBase(FILE_NAME)
 
 	mainWindow.Hide()
 	showDBWindow.Show()
-	fmt.Println("Типа показали базу.")
 }
 
 // Окно добавления тиража
@@ -180,8 +216,16 @@ func setStartInterface() {
 		fyneApp.Quit()
 	})
 
-	showDBWindow.Resize(fyne.NewSize(400, 300))
+	showDBWindow.Resize(fyne.NewSize(890, 500))
 	showDBWindow.CenterOnScreen()
+
+	showDBTable.SetColumnWidth(0, 50)
+	showDBTable.SetColumnWidth(1, 735)
+	showDBTable.SetColumnWidth(2, 85)
+
+	showDBWindow.SetContent(
+		showDBTable,
+	)
 
 	showDBWindow.SetCloseIntercept(func() {
 		showDBWindow.Hide()
